f9a613b11b647e9506ee5b74b027569408f66ca7
Jaya Surya <pjayasurya2711@gmail.com>
2023-09-30 15:52:21 +0530

    just saving

--- client/src/context/api/ApiState.js
+++ client/src/context/api/ApiState.js
@@ -1,54 +1,56 @@
-import ApiContext from './ApiContext';
+import ApiContext from "./ApiContext";
 // import axios from 'axios'
-import axios from 'axios';
+import axios from "axios";
 
 const ApiState = (props) => {
-    const host = 'http://localhost:8000';
+  const host = "http://localhost:8000";
 
-    const login = async (isadmin,creds) => {
+  const login = async (isadmin, creds) => {
+    let reqBody = {
+      email: creds.email,
+      password: creds.password,
+      role: isadmin ? "admin" : "user",
+    };
+    return axios
+      .post(`${host}/api/auth/login`, reqBody, {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      })
+      .then(function (response) {
+        localStorage.setItem("token", response.data.token);
+        return [response.status, false];
+      })
+      .catch(function (error) {
+        console.log(error);
+        return [error.response.status, true];
+      });
+  };
 
-        let reqBody = {
-            "email": creds.email,
-            "password": creds.password,
-            "role": isadmin ? "admin" : "user"
-        }
-        return Axios.post(`${host}/api/auth/login`, reqBody, {
+  const signup = async (creds) => {
+    await axios
+      .post(`${host}/api/auth/createuser`, creds, {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      })
+      .then(function (response) {
+        localStorage.setItem("token", response.data.token);
+        return [response.status, false];
+      })
+      .catch(function (error) {
+        console.log(error);
+        return [error.response.status, true];
+      });
+  };
 
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            }
-        }).then(function (response) {
-            localStorage.setItem("token", response.data.token)
-            return [response.status,false];
-        })
-            .catch(function (error) {
-                console.log(error)
-                return [error.response.status,true];
-            });
-    }
-
-    const signup = async (creds) =>{
-        await axios.post(`${host}/api/auth/createuser`, creds, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-        }).then(function (response) {
-            localStorage.setItem("token", response.data.token)
-            return [response.status,false];
-        })
-            .catch(function (error) {
-                console.log(error)
-                return [error.response.status,true];
-            });
-    }
-
-    return (
-        <ApiContext.Provider value={{ login, signup }}>
-            {props.children}
-        </ApiContext.Provider>
-    )
-}
+  return (
+    <ApiContext.Provider value={{ login, signup }}>
+      {props.children}
+    </ApiContext.Provider>
+  );
+};
 
 export default ApiState;
