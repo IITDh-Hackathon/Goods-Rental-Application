82a9587d21238da0ef8c4639dc6a8427eb0709f8
karthik-k-18 <200010024@iitdh.ac.in>
2023-10-01 18:43:24 +0530

    added endpoint to add citylistings

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -61,11 +61,3 @@ export const getUser = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 }
-export const getAllCategories = async (req, res) => {
-  try {
-    let categories = await Item.find().distinct("category");
-    res.status(200).json(categories);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
