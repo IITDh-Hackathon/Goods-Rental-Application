e8a58cac0001f2d2ac4eb7dc5cf471d62b5e9968
Jaya Surya <pjayasurya2711@gmail.com>
2023-10-02 01:20:56 +0530

    minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
+import { useState } from "react";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
