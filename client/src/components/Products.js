7ba0254ca1129c5cb1575d5a403a3e32df9ca10c
karthikmurakonda <karthik.murakonda14@gmail.com>
2023-10-02 01:39:55 +0530

    products page and added images

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -1,16 +1,59 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react';
 import '../css/ProductsCss.css'
 import GoodsCard from './GoodsCard';
+import ApiContext from '../context/api/ApiContext';
+import Pagination from '@mui/material/Pagination';
+
+
 
 const Products = (props) => {
-    const { category } = props;
     const [selectedCategory, setSelectedCategory] = useState(null);
+    const context = React.useContext(ApiContext);
+    const [items, setItems] = useState([]);
+    const {city} = context;
+    console.log(city);
+    const [params, setParams] = useState({
+        page: 1,
+        limit: 4,
+        category: selectedCategory,
+        city: city,
+    })
+    const { getItems } = context;
+
+    useEffect(() =>
+    {
+        setParams({
+            ...params,
+            city: city,
+        })
+    }
+    , [city]);
+
+    useEffect(() => {
+        getItems(params).then((res) => {
+            setItems(res[0]);
+        }
+        );
+    }, [params, getItems]);
+
 
     // Function to handle category selection
     const handleCategoryClick = (clickedCategory) => {
-        setSelectedCategory(clickedCategory);
+        setParams({
+            ...params,
+            category: clickedCategory,
+            page: 1
+        });
     };
+
+    const handleChangePage = (event, newPage) => {
+        setParams({
+            ...params,
+            page: newPage
+        });
+    }
+
 
     return (
         <div>
@@ -25,7 +68,7 @@ const Products = (props) => {
                                 {['Baskets', 'Furniture', 'Clothing', 'Books', 'Sports', 'Others'].map((item) => (
                                     <div
                                         key={item}
-                                        className={`filter-category-item ${selectedCategory === item ? 'selected-category' : ''}`}
+                                        className={`filter-category-item ${params.category === item ? 'selected-category' : ''}`}
                                         onClick={() => handleCategoryClick(item)}
                                     >
                                         {item}
@@ -36,13 +79,13 @@ const Products = (props) => {
                     </div>
                 </div>
                 <div className="products">
-                    <GoodsCard name="Basket" description="This is a basket" price="100" quantity="10" category="Baskets" message="Add to cart" />
-                    <GoodsCard name="Basket" description="This is a basket" price="100" quantity="10" category="Baskets" message="Add to cart" />
-                    <GoodsCard name="Basket" description="This is a basket" price="100" quantity="10" category="Baskets" message="Add to cart" />
-                    <GoodsCard name="Basket" description="This is a basket" price="100" quantity="10" category="Baskets" message="Add to cart" />
-                    <GoodsCard name="Basket" description="This is a basket" price="100" quantity="10" category="Baskets" message="Add to cart" />
-                    <GoodsCard name="Basket" description="This is a basket" price="100" quantity="10" category="Baskets" message="Add to cart" />
+                    {items.data && items.data.results && items.data.results.map((item) => (
+                        <GoodsCard name={item.name} description={item.description} price={item.price} quantity={item.quantity} category={item.category} images={item.images} key={item.id} id={item.id} />
+                    ))
+                    }
+
                 </div>
+                    <Pagination className='paninate' count={items.data && items.data.totalPages} page={params.page} onChange={handleChangePage} />
             </div>
         </div>
     )
