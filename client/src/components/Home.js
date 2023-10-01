2b3df9592e03977a00f63cca4857e2a582d49ae2
karthikmurakonda <karthik.murakonda14@gmail.com>
2023-10-01 22:23:53 +0530

    merge changes

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,14 @@
 import React from "react";
 import HomeCarousel from "./carousel.js";
 import Categories from "./Categories";
-
+import { Navigate } from "react-router-dom";
+import ApiContext from "../context/api/ApiContext.js";
 const Home = () => {
+  const context = React.useContext(ApiContext);
+  const { profile, loginStatus } = context;
+  if(profile && profile.role === "admin"){
+    return <Navigate to="/admin" />
+  }
   return (
     <>
     <HomeCarousel />
